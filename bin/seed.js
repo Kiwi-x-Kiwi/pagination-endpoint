1c1404cdef41c3a6ab6dc450782f716ad310175f
Qiwei Lin <Qiwei.l.ql@gmail.com>
2019-11-04 19:34:57 -0500

    Removed unnecessary console.logs from seed file

--- bin/seed.js
+++ bin/seed.js
@@ -8,11 +8,7 @@ const errors = []
 async function submitDocuments(name, model, jsonData, drop = true) {
   var documents
   try {
-    console.log('dropping .')
-    console.log(model)
     drop && await model.deleteMany()
-    console.log('creating .')
-    console.log(model)
     documents = await model.create(jsonData)
   } catch (err) {
     errors.push(`${name} - Could not finish populating documents: ${err.name} - ${err.errmsg}`)
@@ -23,8 +19,6 @@ async function submitDocuments(name, model, jsonData, drop = true) {
 }
 
 async function createDBEntries() {
-  console.log('starting to create DB Entries.')
-
   await submitDocuments('apps', App, Array.from({ length: 999 }).map((e, i) => {
     const id = i + 1;
 
@@ -35,17 +29,4 @@ async function createDBEntries() {
   }))
 }
 
-function seed(cb) {
-  cb()
-    .then(() => {
-      console.log('\n\nRan into these errors:')
-      console.log('\n\t' + errors.join('\n\t') + '\n\n')
-      mongoose.disconnect()
-    })
-    .catch(err => {
-      console.log('Error populating the database:  ', err)
-      mongoose.disconnect()
-    })
-}
-
-seed(createDBEntries)
+createDBEntries();
