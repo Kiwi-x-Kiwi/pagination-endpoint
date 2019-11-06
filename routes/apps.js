9015fb84b9b04db79febc4c2a4a911c219cf8f74
Qiwei Lin <Qiwei.l.ql@gmail.com>
2019-11-05 21:28:07 -0500

    Had a better understanding and went back and revised the logic and accounted for more corner cases.

--- routes/apps.js
+++ routes/apps.js
@@ -2,8 +2,48 @@ const express = require('express');
 const router = express.Router();
 const App = require('../models/App')
 
+function getRangeByID(arr, start, end, max){
+  const range = [];
+  let i = 0;
+  arr = arr.sort(sortHelper('id'));
+  start = Math.max(0, start) || 0;
+  end = Math.min(end, Number.MAX_SAFE_INTEGER) || Number.MAX_SAFE_INTEGER;
+
+  while (i < arr.length && range.length < max && arr[i].id <= end){
+    if(arr[i].id >= start) range.push(arr[i])
+    i++;
+  }
+
+  return range;
+}
+
+function getRangeByName(arr, start, end, max){  
+  const nameRegex = /my-app-(\d){3,}/;
+  const range = [];
+  let i = 0;
+  arr = arr.sort(sortHelper('name'));
+  if(start){
+    if(!nameRegex.test(start)) return [];
+    else start = start.match(/\d+/)[0]
+  }else start = 0;
+  if(end){
+    if(!nameRegex.test(end)) end = Number.MAX_SAFE_INTEGER;
+    else end = end.match(/\d+/)[0]
+  }else end = Number.MAX_SAFE_INTEGER;
+  
+
+  while (i < arr.length && range.length < max) {
+    let id = arr[i].name.match(/\d+/)[0];
+    if(id > end) break;
+    else if (id >= start) range.push(arr[i]);
+    i++;
+  }
+
+  return range;
+}
+
 function sortHelper(sortBy) {
-  return function(a, b){
+  return function (a, b) {
     return a[sortBy] - b[sortBy]
   }
 }
@@ -13,24 +53,34 @@ router.get('/', async (req, res, next) => {
   const DEFAULT_SIZE = 50;
   let apps = await App.find().select('-_id -__v')
 
-  if(req.query.range){
+  if (req.query.range) {
     let range = JSON.parse(req.query.range)
-    let {by, start, end, max, order} = range;
+    let { by, start, end, max, order } = range;
 
-    if (!by || (by !== 'name' && by !== 'id')){
-      return res.status(400).send('undefined')
+    if (order && (order != 'asc' && order != 'desc')) return res.status(400).send('undefined');
+    else order = order || 'asc';
+    max = max ? Math.min(max, DEFAULT_SIZE) : DEFAULT_SIZE;
+
+    switch (by) {
+      case 'id':
+        if (start && (!Number.isInteger(start) || start < 0)) return res.status(400).send('undefined');
+        else if (end && (!start || !Number.isInteger(end))) return res.status(400).send('undefined');
+        apps = getRangeByID(apps, start, end, max);
+        break;
+
+      case 'name':
+        if (start && Number.isInteger(start)) return res.status(400).send('undefined');
+        else if (end && (!start || Number.isInteger(end))) return res.status(400).send('undefined');
+        apps = getRangeByName(apps, start, end, max);
+        break;
+
+      default:
+        return res.status(400).send('undefined')
     }
 
-    start = Math.max(start, 1) || 1;
-    max = max ? Math.min(max, DEFAULT_SIZE) : DEFAULT_SIZE;
-    end = Math.min(end, start + max -1) || start + max -1;
-    order = order || 'asc';
+    if(order == 'desc') apps = apps.reverse();
 
-    if(order === 'desc'){
-      apps = apps.sort(sortHelper(by)).reverse().slice(start - 1, end); 
-    } else apps = apps.sort(sortHelper(by)).slice(start - 1, end);
-    
-  }else apps = apps.slice(0, DEFAULT_SIZE)
+  } else apps = apps.sort(sortHelper('id')).slice(0, DEFAULT_SIZE)
 
   res.json(apps);
 });
